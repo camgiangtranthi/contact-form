d3290f24fa5a2ba3e0be6856b2fcf7a36a70b65a
Giang <gtran527@gmail.com>
2023-03-11 16:49:05 +0700

    contact form

---
+++ front-end/script.js
@@ -0,0 +1,28 @@
+const form = document.querySelector("form");
+const statusTxt = form.querySelector(".button-area span")
+
+form.onsubmit = (e) => {
+    e.preventDefault();
+    statusTxt.style.display = "block";
+
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST", "../back-end/message.php", true);
+    xhr.onload = () => {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            let response = xhr.response;
+            if (response.indexOf("Email and password field is required!") !== -1
+                || response.indexOf("Enter a valid email address!")
+                || response.indexOf("Sorry, failed to send your message!"))
+            {
+                statusTxt.style.color = "red";
+            }
+            form.reset()
+            setTimeout(() => {
+                statusTxt.style.display = "none";
+            }, 3000)
+            statusTxt.innerText = response;
+        }
+    }
+    let formData = new FormData()
+    xhr.send(formData);
+}
